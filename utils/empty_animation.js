9c14285f0acd90f93331645056e0c4c72a0989f0
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-25 12:05:31 +0530

    fix: resolved image path

--- utils/empty_animation.js
+++ utils/empty_animation.js
@@ -2,7 +2,7 @@ export function empty_animation(filterMode)
 {
     return `
         <div class="place">
-            <img src="../assets/images/rolling-bush.png" alt="bush" class="bush">
+            <img src="./assets/images/rolling-bush.png" alt="bush" class="bush">
             <div class="empty-filter-container">
                 <div class="empty-filter">No ${(filterMode === 'all') ? '' : filterMode} tasks left</div>
             </div>
